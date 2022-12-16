efa221de444204f65aaebff0770c93e80e828fae
Vika <sagitovavika2016@gmail.com>
2022-12-16 23:53:50 +0200

    add gateway

---
+++ gateway/src/app.module.ts
@@ -0,0 +1,32 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import 'dotenv/config';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'albums',
+              url: `http://localhost:${process.env.PORT_ALBUMS}/graphql`,
+            },
+            // {
+            //   name: 'artists',
+            //   url: `http://localhost:${process.env.PORT_ARTISTS}`,
+            // },
+          ],
+        }),
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
